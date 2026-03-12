6bf49c67b17147414f1d452dc3e5e40bd50375df
Sqealbert <hddenherder@gmail.com>
2026-03-12 10:44:50 -0500

    Added pushable boxes (very buggy at the moment)

--- scripts/player.gd
+++ scripts/player.gd
@@ -10,4 +10,14 @@ func get_input():
 func _physics_process(delta: float) -> void:
 	get_input()
 	velocity = speed * direction
+	
+	var push_force = 80.0
+	
+	for block in get_slide_collision_count():
+		var collision = get_slide_collision(block)
+		var collision_block = collision.get_collider()
+		if collision_block.is_in_group("Blocks"):# and abs(collision_block.get_linear_velocity()) < MAX_VELOCITY:
+			collision_block.apply_central_impulse(collision.get_normal() * push_force)
+		
+	
 	move_and_slide()
